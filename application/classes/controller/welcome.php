621bb8e63fb6c36b46b78203981383f01c9652b1
mamrenko <Alex@iMac-Alexandra.local>
2013-09-21 22:11:57 +0400

    еще раз какая то хрень

--- application/classes/controller/welcome.php
+++ application/classes/controller/welcome.php
@@ -1,10 +1,18 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Welcome extends Controller {
-
+class Controller_Welcome extends Controller_Application{
+      
 	public function action_index()
 	{
-		$this->request->response = 'hello, world!';
+       
+		
+		$content = View::factory('welcome')
+			->bind('random', $random);
+		$random = rand(1, 10);
+
+		
+
+		$this->template->content = $content;
 	}
 
 } // End Welcome
