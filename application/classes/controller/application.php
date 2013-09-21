f963dec581bc96f03fe68db32f9212c11da11a2b
mamrenko <Alex@iMac-Alexandra.local>
2013-09-21 21:13:36 +0400

    add application

---
+++ application/classes/controller/application.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+ abstract class Controller_Application extends Controller_Template{
+      
+	public function before() {
+            parent::before();
+            View::bind_global('site_name', $site_name);
+            $site_name = 'Egotist Beta';
+            
+            $this->template->content = '';
+            $this->template->styles = array('reset', 'common');
+            $this->template->scripts = array();
+        }
+}
+
