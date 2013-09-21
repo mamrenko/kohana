f3a345032dddf97d2cafd670c02f1da9f49c649c
mamrenko <Alex@iMac-Alexandra.local>
2013-09-21 22:28:03 +0400

    add media folder and file

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,7 +1,6 @@
-<?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<h2>About Egotist 1111111111111</h2>
+<p>We could go on and on about ourselves, 
+    and we think you could to!  That is why we build this 
+    wonderful application, so people like you could tell 
+    the world every little thing you were doing.</p>
