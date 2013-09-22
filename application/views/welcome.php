dd5dba95977c3c312fc7035c18ae0b717502a670
mamrenko <Alex@iMac-Alexandra.local>
2013-09-22 20:43:49 +0400

    arr::pluck method

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -10,3 +10,26 @@
 ));?>
 <p><?= URL::title('Narwayls are real, - "Breaking News"');?></p>
 <p><?= URL::base()?></p>
+<?php
+$array_1 = array(
+'first_name' => 'Joe',
+'last_name' => 'User',
+);
+
+$array_2 = array(
+'first_name' => 'Jason',
+'last_name' => 'User2',
+);
+
+$array_3 = array(
+'first_name' => 'Joerea',
+'last_name' => 'User3',
+);
+$arrayList = array($array_1, $array_2, $array_3);
+$first_name = Arr::pluck($arrayList, 'first_name');
+
+?>
+<?foreach($first_name as $first):?>
+<p><?=$first?></p>
+<?endforeach?>
+
